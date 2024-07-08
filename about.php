33a2fe71fba2aaebd30c54662fe2ad15b9e3d628
zetski <batuigasjerichodesu@gmail.com>
2024-07-08 11:43:32 +0800

    save

--- about.php
+++ about.php
@@ -3,6 +3,20 @@
         <div class="card rounded-0">
             <div class="card-body">
                 <?= htmlspecialchars_decode(file_get_contents('./about.html')) ?>
+                <div style="margin-left: 50px">
+    <h5>Contact Us</h5>
+    <dl>
+        <dt>Email</dt>
+        <dd><?= $_settings->info('email') ?></dd>
+        <dt>Telephone No</dt>
+        <dd><?= $_settings->info('phone') ?></dd>
+        <dt>Mobile No</dt>
+        <dd><?= $_settings->info('mobile') ?></dd>
+        <dt>Address</dt>
+        <dd><?= $_settings->info('address') ?></dd>
+    </dl>
+</div>
+
             </div>
         </div>
     </div>
