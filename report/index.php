e652aa96d332fd47f3b6a59f992423220a94f0c6
zetski <batuigasjerichodesu@gmail.com>
2024-07-20 21:22:53 +0800

    save

--- report/index.php
+++ report/index.php
@@ -31,14 +31,16 @@
                                 </div>
                                 <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                     <label for="message" class="control-label">Message <small class="text-danger">*</small></label>
-                                    <textarea rows="3" class="form-control form-control-sm rounded-0" name="message" id="message" required="required" style="padding-right: 40px;"></textarea>
-                                </div>
-                                <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                    <label for="capture" class="control-label">Take a Picture</label>
                                     <div class="position-relative">
-                                        <video id="video" width="100%" height="100%" autoplay></video>
-                                        <button type="button" id="capture" class="btn btn-primary">Capture</button>
-                                        <canvas id="canvas" class="d-none"></canvas>
+                                        <textarea rows="3" class="form-control form-control-sm rounded-0" name="message" id="message" required="required" style="padding-right: 40px;"></textarea>
+                                        <label class="upload-icon" for="image-upload">
+                                            <i class="fa fa-camera"></i>
+                                        </label>
+                                        <input type="file" class="d-none" id="image-upload" name="image" accept="image/*">
+                                        <div id="image-preview-container" class="d-none">
+                                            <img id="image-preview" src="#" alt="Image Preview" class="img-thumbnail">
+                                            <span id="remove-image" class="remove-image"><i class="fa fa-times"></i></span>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -72,7 +74,6 @@
                                         <option value="Ticad">Ticad</option>
                                     </select>
                                 </div>
-                                <input type="hidden" name="captured_image" id="captured_image">
                             </form>
                         </div>
                     </div>
@@ -86,59 +87,68 @@
     </div>
 </section>
 
-<script>
-    const video = document.getElementById('video');
-    const captureButton = document.getElementById('capture');
-    const canvas = document.getElementById('canvas');
-    const capturedImageInput = document.getElementById('captured_image');
-
-    // Get access to the camera
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-            video.srcObject = stream;
-            video.play();
-        });
-    }
-
-    // Capture the image from the video feed
-    captureButton.addEventListener('click', function () {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        const context = canvas.getContext('2d');
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        const dataURL = canvas.toDataURL('image/png');
-        capturedImageInput.value = dataURL;
-        // Optionally display the captured image
-        Swal.fire({
-            imageUrl: dataURL,
-            imageAlt: 'Captured Image',
-        });
-    });
-
-    // Form submission handler
-    document.getElementById('request-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        // Add additional form handling code here if needed
-        this.submit();
-    });
-</script>
+<!-- Modal -->
+<div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="imageModalLabel">Image Preview</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img id="modal-image" src="#" alt="Full-size Image" class="img-fluid">
+      </div>
+    </div>
+  </div>
+</div>
 
 <style>
+    body {
+        padding-top: 10px;
+        margin-top: 40px;
+    }
     .position-relative {
         position: relative;
     }
-    #video {
-        width: 100%;
-        height: auto;
+    .upload-icon {
+        position: absolute;
+        right: 10px;
+        bottom: 10px;
+        cursor: pointer;
+        font-size: 1.2rem;
+        color: #6c757d;
     }
-    #canvas {
-        display: none;
-    }
-    #capture {
+    #image-preview-container {
         position: absolute;
         bottom: 10px;
-        left: 50%;
-        transform: translateX(-50%);
-        z-index: 10;
+        right: 50px;
+        width: 50px;
+        height: 50px;
+        overflow: hidden;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    #image-preview {
+        max-width: 100%;
+        max-height: 100%;
+        cursor: pointer;
+    }
+    .remove-image {
+        position: absolute;
+        top: 0;
+        right: 0;
+        background-color: rgba(255, 255, 255, 0.7);
+        border-radius: 50%;
+        cursor: pointer;
+        padding: 2px;
     }
 </style>
+
+<script>
+    document.getElementById('contact').addEventListener('input', function (e) {
+        this.value = this.value.replace(/[^0-9]/g, '').slice(0, 11);
+    });
+</script>
+
+<script src="report/script.js"></script>
