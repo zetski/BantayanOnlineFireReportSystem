63f941bed7db93c4bcb244f8615d5938eb66fab3
zetski <batuigasjerichodesu@gmail.com>
2024-07-22 08:04:31 +0800

    save

--- report/script.js
+++ report/script.js
@@ -1,12 +1,16 @@
 $(function(){
+    console.log(_base_url_);
     $('#request-form').submit(function(e){
         e.preventDefault();
         var _this = $(this);
         $('.err-msg').remove();
         start_loader();
+        var formData = new FormData(this);
+        console.log("Form Data:", formData);
+
         $.ajax({
-            url: _base_url_+"classes/Master.php?f=save_request",
-            data: new FormData(this),
+            url: _base_url_ + "classes/Master.php?f=save_request",
+            data: formData,
             cache: false,
             contentType: false,
             processData: false,
@@ -14,11 +18,12 @@ $(function(){
             type: 'POST',
             dataType: 'json',
             error: function(err) {
-                console.log(err);
+                console.log("AJAX error:", err);
                 alert_toast("An error occurred",'error');
                 end_loader();
             },
             success: function(resp) {
+                console.log("AJAX response:", resp);
                 if (typeof resp == 'object' && resp.status == 'success') {
                     location.reload();
                 } else if (resp.status == 'failed' && !!resp.msg) {
@@ -31,7 +36,7 @@ $(function(){
                 } else {
                     alert_toast("An error occurred",'error');
                     end_loader();
-                    console.log(resp);
+                    console.log("Unexpected response:", resp);
                 }
             }
         });
