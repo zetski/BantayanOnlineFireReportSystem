ec9c8692c3deca5f2819f2a81b920a37204652e9
zetski <batuigasjerichodesu@gmail.com>
2024-07-22 08:37:17 +0800

    save

--- classes/Master.php
+++ classes/Master.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once('../config.php');
 Class Master extends DBConnection {
 	private $settings;
