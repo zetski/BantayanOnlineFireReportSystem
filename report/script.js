6fed9db03c2650a208fa9a277a4c58ee23884f4d
zetski <batuigasjerichodesu@gmail.com>
2024-07-23 22:06:50 +0800

    SAVE

--- report/script.js
+++ report/script.js
@@ -1,42 +1,43 @@
 $(function(){
+    // Form submission handling
     $('#request-form').submit(function(e){
         e.preventDefault();
         var _this = $(this);
         $('.err-msg').remove();
         start_loader();
+
         $.ajax({
-            url: _base_url_+"classes/Master.php?f=save_request",
+            url: _base_url_ + "classes/Master.php?f=save_request",
             data: new FormData(this),
             cache: false,
             contentType: false,
             processData: false,
             method: 'POST',
-            type: 'POST',
             dataType: 'json',
             error: function(err) {
                 console.log(err);
-                alert_toast("An error occurred",'error');
+                alert_toast("An error occurred", 'error');
                 end_loader();
             },
             success: function(resp) {
-                if (typeof resp == 'object' && resp.status == 'success') {
+                if (typeof resp === 'object' && resp.status === 'success') {
                     location.reload();
-                } else if (resp.status == 'failed' && !!resp.msg) {
+                } else if (resp.status === 'failed' && resp.msg) {
                     var el = $('<div>');
                     el.addClass("alert alert-danger err-msg").text(resp.msg);
                     _this.prepend(el);
                     el.show('slow');
                     $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
-                    end_loader();
                 } else {
-                    alert_toast("An error occurred",'error');
-                    end_loader();
+                    alert_toast("An error occurred", 'error');
                     console.log(resp);
                 }
+                end_loader();
             }
         });
     });
 
+    // Image upload preview handling
     $('#image-upload').change(function(){
         var input = this;
         if (input.files && input.files[0]) {
@@ -50,10 +51,12 @@ $(function(){
         }
     });
 
+    // Show image in modal on click
     $(document).on('click', '#image-preview', function() {
         $('#imageModal').modal('show');
     });
 
+    // Remove image and reset preview
     $(document).on('click', '#remove-image', function() {
         $('#image-upload').val('');
         $('#image-preview').attr('src', '#');
