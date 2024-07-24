e23f33631c2d039dde92872b2c92e1e722c42225
zetski <batuigasjerichodesu@gmail.com>
2024-07-24 08:53:41 +0800

    save

--- report/index.php
+++ report/index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fire Reporting</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body>
 <section class="py-3">
     <div class="container">
         <div class="content py-3 px-3" style="background-color: #2980B9">
@@ -21,6 +31,7 @@
                             <?php endif;?>
                             <form action="" id="request-form" enctype="multipart/form-data">
                                 <input type="hidden" name="id">
+                                <input type="hidden" name="image" id="image-data">
                                 <div class="form-group col-lg-6 col-md-8 col-sm-12 col-xs-12">
                                     <label for="fullname" class="control-label">Fullname <small class="text-danger">*</small></label>
                                     <input type="text" class="form-control form-control-sm rounded-0" name="fullname" id="fullname" required="required">
@@ -33,10 +44,9 @@
                                     <label for="message" class="control-label">Message <small class="text-danger">*</small></label>
                                     <div class="position-relative">
                                         <textarea rows="3" class="form-control form-control-sm rounded-0" name="message" id="message" required="required" style="padding-right: 40px;"></textarea>
-                                        <label class="upload-icon" for="image-upload">
-                                            <i class="fa fa-camera"></i>
-                                        </label>
-                                        <input type="file" class="d-none" id="image-upload" name="image" accept="image/*">
+                                        <button type="button" class="btn btn-primary" id="camera-btn">Take Picture</button>
+                                        <video id="video" width="320" height="240" autoplay class="d-none"></video>
+                                        <canvas id="canvas" width="320" height="240" class="d-none"></canvas>
                                         <div id="image-preview-container" class="d-none">
                                             <img id="image-preview" src="#" alt="Image Preview" class="img-thumbnail">
                                             <span id="remove-image" class="remove-image"><i class="fa fa-times"></i></span>
@@ -47,31 +57,7 @@
                                     <label for="location" class="control-label">Location <small class="text-danger">*</small></label>
                                     <select class="form-control form-control-sm rounded-0" name="location" id="location" required="required">
                                         <option value="">Select Barangay</option>
-                                        <option value="Atop-Atop">Atop-Atop</option>
-                                        <option value="Baigad">Baigad</option>
-                                        <option value="Bantigue">Bantigue</option>
-                                        <option value="Baod">Baod</option>
-                                        <option value="Binaobao">Binaobao</option>
-                                        <option value="Botigues">Botigues</option>
-                                        <option value="Doong">Doong</option>
-                                        <option value="Guiwanon">Guiwanon</option>
-                                        <option value="Hilotongan">Hilotongan</option>
-                                        <option value="Kabac">Kabac</option>
-                                        <option value="Kabangbang">Kabangbang</option>
-                                        <option value="Kampinganon">Kampinganon</option>
-                                        <option value="Kangkaibe">Kangkaibe</option>
-                                        <option value="Lipayran">Lipayran</option>
-                                        <option value="Luyongbay-bay">Luyongbay-bay</option>
-                                        <option value="Mojon">Mojon</option>
-                                        <option value="Oboob">Oboob</option>
-                                        <option value="Patao">Patao</option>
-                                        <option value="Putian">Putian</option>
-                                        <option value="Sillon">Sillon</option>
-                                        <option value="Suba">Suba</option>
-                                        <option value="Sulangan">Sulangan</option>
-                                        <option value="Sungko">Sungko</option>
-                                        <option value="Tamiao">Tamiao</option>
-                                        <option value="Ticad">Ticad</option>
+                                        <!-- Add barangay options here -->
                                     </select>
                                 </div>
                             </form>
@@ -103,10 +89,6 @@
 </div>
 
 <style>
-    body {
-        padding-top: 10px;
-        margin-top: 40px;
-    }
     .position-relative {
         position: relative;
     }
@@ -149,6 +131,52 @@
     document.getElementById('contact').addEventListener('input', function (e) {
         this.value = this.value.replace(/[^0-9]/g, '').slice(0, 11);
     });
+
+    document.getElementById('camera-btn').addEventListener('click', function () {
+        var video = document.getElementById('video');
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+
+        video.classList.remove('d-none');
+        canvas.classList.add('d-none');
+
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function (err) {
+                console.error("Error: " + err);
+            });
+
+        video.addEventListener('click', function () {
+            context.drawImage(video, 0, 0, 320, 240);
+            canvas.classList.remove('d-none');
+            video.classList.add('d-none');
+            video.srcObject.getTracks().forEach(track => track.stop());
+
+            var dataURL = canvas.toDataURL('image/png');
+            document.getElementById('image-data').value = dataURL;
+            document.getElementById('image-preview').src = dataURL;
+            document.getElementById('modal-image').src = dataURL;
+            document.getElementById('image-preview-container').classList.remove('d-none');
+        });
+    });
+
+    document.getElementById('remove-image').addEventListener('click', function () {
+        document.getElementById('image-data').value = '';
+        document.getElementById('image-preview').src = '#';
+        document.getElementById('modal-image').src = '#';
+        document.getElementById('image-preview-container').classList.add('d-none');
+    });
+
+    document.getElementById('image-preview').addEventListener('click', function () {
+        $('#imageModal').modal('show');
+    });
 </script>
 
-<script src="report/script.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
