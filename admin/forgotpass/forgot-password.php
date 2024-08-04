81a5a01043e49e0e815b0de168a3e18bbd78165a
zetski <batuigasjerichodesu@gmail.com>
2024-08-04 21:16:26 +0800

    save

--- admin/forgotpass/forgot-password.php
+++ admin/forgotpass/forgot-password.php
@@ -10,57 +10,7 @@
 
     <title>Online Fire Reporting System</title>
   </head>
-  <body class="hold-transition login-page">
-  <script>
-    start_loader()
-  </script>
-  <style>
-    body {
-      background-image: url("<?php echo validate_image($_settings->info('cover')) ?>");
-      background-size: cover;
-      background-repeat: no-repeat;
-      backdrop-filter: contrast(1);
-    }
-    #page-title {
-      text-shadow: 6px 4px 7px black;
-      font-size: 3.5em;
-      color: #fff4f4 !important;
-      background: #8080801c;
-    }
-  </style>
-  <h1 class="text-center text-white px-4 py-5" id="page-title"><b><?php echo $_settings->info('name') ?></b></h1>
-  <div class="login-box">
-    <div class="card card-danger my-2">
-      <div class="card-body">
-        <p class="login-box-msg">Enter your email to reset your password</p>
-        <form id="forgot-password-frm" action="forgot-password-process.php" method="post">
-          <div class="input-group mb-3">
-            <input type="email" class="form-control" name="email" placeholder="Email" required>
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <button type="submit" class="btn btn-primary btn-block">Send Reset Link</button>
-            </div>
-          </div>
-        </form>
-        <p class="mb-1 mt-3">
-          <a href="login.php">Back to Login</a>
-        </p>
-      </div>
-    </div>
-  </div>
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="dist/js/adminlte.min.js"></script>
-  <script>
-    $(document).ready(function(){
-      end_loader();
-    });
-  </script>
-</body>
+  <body>
+        <h1>hello</h1>
+  </body>
 </html>
