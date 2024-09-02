397777b83c333b9f4279abc2c74a69a543e3bf2d
zetski <batuigasjerichodesu@gmail.com>
2024-09-02 14:43:28 +0800

    save

--- report/script.js
+++ report/script.js
@@ -5,59 +5,36 @@ $(function(){
         $('.err-msg').remove();
         start_loader();
         $.ajax({
-            url: _base_url_+"classes/Master.php?f=save_request",
-            data: new FormData(this),
+            url: _base_url_ + "classes/Master.php?f=save_request",
+            data: new FormData($(this)[0]),
             cache: false,
             contentType: false,
             processData: false,
             method: 'POST',
             type: 'POST',
             dataType: 'json',
-            error: function(err) {
+            error: err => {
                 console.log(err);
-                alert_toast("An error occurred",'error');
+                alert_toast("An error occurred", 'error');
                 end_loader();
             },
             success: function(resp) {
                 if (typeof resp == 'object' && resp.status == 'success') {
                     location.reload();
                 } else if (resp.status == 'failed' && !!resp.msg) {
-                    var el = $('<div>');
-                    el.addClass("alert alert-danger err-msg").text(resp.msg);
+                    var el = $('<div>')
+                        .addClass("alert alert-danger err-msg")
+                        .text(resp.msg);
                     _this.prepend(el);
                     el.show('slow');
                     $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
                     end_loader();
                 } else {
-                    alert_toast("An error occurred",'error');
+                    alert_toast("An error occurred", 'error');
                     end_loader();
                     console.log(resp);
                 }
             }
         });
-    });
-
-    $('#image-upload').change(function(){
-        var input = this;
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                $('#image-preview').attr('src', e.target.result);
-                $('#modal-image').attr('src', e.target.result);
-                $('#image-preview-container').removeClass('d-none');
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    });
-
-    $(document).on('click', '#image-preview', function() {
-        $('#imageModal').modal('show');
-    });
-
-    $(document).on('click', '#remove-image', function() {
-        $('#image-upload').val('');
-        $('#image-preview').attr('src', '#');
-        $('#image-preview-container').addClass('d-none');
-        $('#modal-image').attr('src', '#');
     });
 });
