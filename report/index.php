f0557bb3e32b8921b3d3905f565e17fc915aec8d
zetski <batuigasjerichodesu@gmail.com>
2024-09-11 07:18:05 +0800

    save

--- report/index.php
+++ report/index.php
@@ -21,61 +21,62 @@
                             <?php endif;?>
                             <form action="" id="request-form" enctype="multipart/form-data">
                                 <input type="hidden" name="id">
-                                <div class="form-group col-lg-6 col-md-8 col-sm-12 col-xs-12">
-                                    <label for="fullname" class="control-label">Fullname <small class="text-danger">*</small></label>
-                                    <input type="text" class="form-control form-control-sm rounded-0" name="fullname" id="fullname" required="required">
+                                
+                                <!-- Lastname, Firstname, Middlename -->
+                                <div class="form-group col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                                    <label for="lastname" class="control-label">Lastname <small class="text-danger">*</small></label>
+                                    <input type="text" class="form-control form-control-sm rounded-0" name="lastname" id="lastname" required="required">
                                 </div>
+                                <div class="form-group col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                                    <label for="firstname" class="control-label">Firstname <small class="text-danger">*</small></label>
+                                    <input type="text" class="form-control form-control-sm rounded-0" name="firstname" id="firstname" required="required">
+                                </div>
+                                <div class="form-group col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                                    <label for="middlename" class="control-label">Middlename</label>
+                                    <input type="text" class="form-control form-control-sm rounded-0" name="middlename" id="middlename">
+                                </div>
+
+                                <!-- Contact -->
                                 <div class="form-group col-lg-6 col-md-8 col-sm-12 col-xs-12">
                                     <label for="contact" class="control-label">Contact # <small class="text-danger">*</small></label>
                                     <input type="text" class="form-control form-control-sm rounded-0" name="contact" id="contact" required="required" maxlength="11" pattern="\d{11}" title="Please enter 11 digits">
                                 </div>
+
+                                <!-- Message -->
                                 <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                     <label for="message" class="control-label">Message <small class="text-danger">*</small></label>
-                                    <div class="position-relative">
-                                        <textarea rows="3" class="form-control form-control-sm rounded-0" name="message" id="message" required="required" style="padding-right: 40px;"></textarea>
-                                        <!-- Removed camera icon -->
-                                        <!-- <label class="upload-icon" for="image-upload">
-                                            <i class="fa fa-camera"></i>
-                                        </label> -->
-                                        <!-- Removed file input -->
-                                        <!-- <input type="file" class="d-none" id="image-upload" name="image" accept="image/*"> -->
-                                        <!-- Removed preview container -->
-                                        <!-- <div id="image-preview-container" class="d-none">
-                                            <img id="image-preview" src="#" alt="Image Preview" class="img-thumbnail">
-                                            <span id="remove-image" class="remove-image"><i class="fa fa-times"></i></span>
-                                        </div> -->
-                                    </div>
+                                    <textarea rows="3" class="form-control form-control-sm rounded-0" name="message" id="message" required="required"></textarea>
                                 </div>
+
+                                <!-- Photo Upload -->
                                 <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                    <label for="location" class="control-label">Location <small class="text-danger">*</small></label>
-                                    <select class="form-control form-control-sm rounded-0" name="location" id="location" required="required">
-                                        <option value="">Select Barangay</option>
-                                        <option value="Atop-Atop">Atop-Atop</option>
-                                        <option value="Baigad">Baigad</option>
-                                        <option value="Bantigue">Bantigue</option>
-                                        <option value="Baod">Baod</option>
-                                        <option value="Binaobao">Binaobao</option>
-                                        <option value="Botigues">Botigues</option>
-                                        <option value="Doong">Doong</option>
-                                        <option value="Guiwanon">Guiwanon</option>
-                                        <option value="Hilotongan">Hilotongan</option>
-                                        <option value="Kabac">Kabac</option>
-                                        <option value="Kabangbang">Kabangbang</option>
-                                        <option value="Kampinganon">Kampinganon</option>
-                                        <option value="Kangkaibe">Kangkaibe</option>
-                                        <option value="Lipayran">Lipayran</option>
-                                        <option value="Luyongbay-bay">Luyongbay-bay</option>
-                                        <option value="Mojon">Mojon</option>
-                                        <option value="Oboob">Oboob</option>
-                                        <option value="Patao">Patao</option>
-                                        <option value="Putian">Putian</option>
-                                        <option value="Sillon">Sillon</option>
-                                        <option value="Suba">Suba</option>
-                                        <option value="Sulangan">Sulangan</option>
-                                        <option value="Sungko">Sungko</option>
-                                        <option value="Tamiao">Tamiao</option>
-                                        <option value="Ticad">Ticad</option>
+                                    <label for="image" class="control-label">Upload Photo (JPEG/JPG only) <small class="text-danger">*</small></label>
+                                    <input type="file" class="form-control form-control-sm rounded-0" name="image" id="image" accept=".jpeg, .jpg" required="required">
+                                </div>
+
+                                <!-- Municipality -->
+                                <div class="form-group col-lg-6 col-md-8 col-sm-12 col-xs-12">
+                                    <label for="municipality" class="control-label">Municipality <small class="text-danger">*</small></label>
+                                    <select class="form-control form-control-sm rounded-0" name="municipality" id="municipality" required="required">
+                                        <option value="">Select Municipality</option>
+                                        <option value="Bantayan">Bantayan</option>
+                                        <option value="Santa Fe">Santa Fe</option>
+                                        <option value="Madridejos">Madridejos</option>
                                     </select>
+                                </div>
+
+                                <!-- Barangay -->
+                                <div class="form-group col-lg-6 col-md-8 col-sm-12 col-xs-12">
+                                    <label for="barangay" class="control-label">Barangay <small class="text-danger">*</small></label>
+                                    <select class="form-control form-control-sm rounded-0" name="barangay" id="barangay" required="required">
+                                        <option value="">Select Barangay</option>
+                                    </select>
+                                </div>
+
+                                <!-- Purok/Street -->
+                                <div class="form-group col-lg-6 col-md-8 col-sm-12 col-xs-12">
+                                    <label for="sitio_street" class="control-label">Purok/Street <small class="text-danger">*</small></label>
+                                    <input type="text" class="form-control form-control-sm rounded-0" name="sitio_street" id="sitio_street" required="required">
                                 </div>
                             </form>
                         </div>
@@ -90,20 +91,6 @@
     </div>
 </section>
 
-<!-- Modal -->
-<div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="imageModalLabel">Image Preview</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <img id="modal-image" src="#" alt="Full-size Image" class="img-fluid">
-      </div>
-    </div>
-  </div>
-</div>
 
 <style>
     body {
@@ -113,46 +100,52 @@
     .position-relative {
         position: relative;
     }
-    /* Removed styling for the upload icon */
-    /* .upload-icon {
-        position: absolute;
-        right: 10px;
-        bottom: 10px;
-        cursor: pointer;
-        font-size: 1.2rem;
-        color: #6c757d;
-    } */
-    /* Removed styling for image preview */
-    /* #image-preview-container {
-        position: absolute;
-        bottom: 10px;
-        right: 50px;
-        width: 50px;
-        height: 50px;
-        overflow: hidden;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    } */
-    /* #image-preview {
-        max-width: 100%;
-        max-height: 100%;
-        cursor: pointer;
-    } */
-    /* .remove-image {
-        position: absolute;
-        top: 0;
-        right: 0;
-        background-color: rgba(255, 255, 255, 0.7);
-        border-radius: 50%;
-        cursor: pointer;
-        padding: 2px;
-    } */
 </style>
 
 <script>
+    document.getElementById('lastname').addEventListener('input', function(e) {
+                e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g, '').replace(/\b\w/g, function(char) {
+                    return char.toUpperCase();
+                });
+            });
+
+            document.getElementById('firstname').addEventListener('input', function(e) {
+                e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g, '').replace(/\b\w/g, function(char) {
+                    return char.toUpperCase();
+                });
+            });
+
+            document.getElementById('middlename').addEventListener('input', function(e) {
+                e.target.value = e.target.value.replace(/[^a-zA-Z\s]/g, '').replace(/\b\w/g, function(char) {
+                    return char.toUpperCase();
+                });
+            });
+
     document.getElementById('contact').addEventListener('input', function (e) {
         this.value = this.value.replace(/[^0-9]/g, '').slice(0, 11);
+    });
+
+    const barangays = {
+        "Bantayan": ["Atop-Atop", "Baigad", "Bantigue", "Baod", "Binaobao", "Botigues", "Doong", "Guiwanon", "Hilotongan", "Kabac", "Kabangbang", "Kampinganon", "Kangkaibe", "Lipayran", "Luyongbay-bay", "Mojon", "Oboob", "Patao", "Putian", "Sillon", "Suba", "Sulangan", "Sungko", "Tamiao", "Ticad"],
+
+        "Santa Fe": ["Balidbid", "Hagdan", "Hilantagaan", "Kinatarkan", "Langub", "Maricaban", "Okoy", "Poblacion", "Pooc", "Talisay"],
+
+        "Madridejos": ["Bunakan", "Kangwayan", "Kaongkod", "Kodia", "Maalat", "Malbago", "Mancilang", "Pili", "Poblacion", "San Agustin", "Tabagak", "Talangnan", "Tarong", "Tugas"]
+    };
+
+    document.getElementById('municipality').addEventListener('change', function () {
+        const municipality = this.value;
+        const barangaySelect = document.getElementById('barangay');
+        barangaySelect.innerHTML = '<option value="">Select Barangay</option>';
+
+        if (barangays[municipality]) {
+            barangays[municipality].forEach(barangay => {
+                const option = document.createElement('option');
+                option.value = barangay;
+                option.textContent = barangay;
+                barangaySelect.appendChild(option);
+            });
+        }
     });
 </script>
 
