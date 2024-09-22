2a8b549f652aad8ef1390d87ae2c456b94cf36cc
zetski <batuigasjerichodesu@gmail.com>
2024-09-22 18:50:35 +0800

    save

--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,6 @@
-<?php require_once('../config.php') ?>
+<?php 
+require_once('../initialize.php');
+require_once('../config.php'); ?>
 <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
 <?php require_once('inc/header.php') ?>
