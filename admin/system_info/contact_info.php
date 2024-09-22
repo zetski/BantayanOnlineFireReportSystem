767fde0d6e2573a4b5093d28db2f866040e0f0b0
zetski <batuigasjerichodesu@gmail.com>
2024-09-22 12:51:07 +0800

    save

--- admin/system_info/contact_info.php
+++ admin/system_info/contact_info.php
@@ -1,9 +1,9 @@
 <?php if($_settings->chk_flashdata('success')): ?>
 <script>
-    alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+    alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success');
 </script>
 <?php endif; ?>
-<div class="content py-3 px-3" style="color: #fff; background-color: #2980B9">
+<div class="content py-3 px-3" style="color: #fff; background-color: #ff4600">
     <h2><b>Contact information</b></h2>
 </div>
 <div class="row mt-lg-n4 mt-md-n4 justify-content-center">
@@ -13,19 +13,34 @@
                 <form action="" id="system-frm">
                     <div id="msg" class="form-group"></div>
                     <div class="form-group">
-                        <label for="phone" class="control-label">Telephone #</label>
-                        <input type="text" class="form-control form-control-sm rounded-0" name="phone" id="phone" value="<?php echo $_settings->info('phone') ?>">
+                        <label for="phone" class="control-label">
+                            <i class="fas fa-phone-alt"></i> Telephone #
+                        </label>
+                        <input type="text" class="form-control form-control-sm rounded-0" name="phone" id="phone" value="<?php echo $_settings->info('phone') ?>" maxlength="15" pattern="[\d\(\)\-]+" title="Please enter a valid phone number">
                     </div>
                     <div class="form-group">
-                        <label for="mobile" class="control-label">Mobile #</label>
+                        <label for="mobile" class="control-label">
+                            <i class="fas fa-mobile-alt"></i> Mobile #
+                        </label>
                         <input type="text" class="form-control form-control-sm rounded-0" name="mobile" id="mobile" value="<?php echo $_settings->info('mobile') ?>" maxlength="11" pattern="\d{11}" title="Please enter exactly 11 digits">
                     </div>
                     <div class="form-group">
-                        <label for="email" class="control-label">Email</label>
+                        <label for="email" class="control-label">
+                            <i class="fas fa-envelope"></i> Email
+                        </label>
                         <input type="email" class="form-control form-control-sm rounded-0" name="email" id="email" value="<?php echo $_settings->info('email') ?>">
                     </div>
+                    <!-- New Facebook field with icon -->
                     <div class="form-group">
-                        <label for="address" class="control-label">Address</label>
+                        <label for="facebook" class="control-label">
+                            <i class="fab fa-facebook-square"></i> Facebook
+                        </label>
+                        <input type="url" class="form-control form-control-sm rounded-0" name="facebook" id="facebook" value="<?php echo $_settings->info('facebook') ?>" placeholder="https://www.facebook.com/yourprofile">
+                    </div>
+                    <div class="form-group">
+                        <label for="address" class="control-label">
+                            <i class="fas fa-map-marker-alt"></i> Address
+                        </label>
                         <textarea rows="3" class="form-control form-control-sm rounded-0" name="address" id="address"><?php echo $_settings->info('address') ?></textarea>
                     </div>
                 </form>
@@ -60,31 +75,37 @@
                     alert_toast("An error occurred", 'error');
                     end_loader();
                 },
-                success: function(resp){
-                    if(typeof resp === 'object' && resp.status === 'success'){
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Success',
-                            text: 'Information updated successfully!',
-                            showConfirmButton: false,
-                            timer: 1500
-                        }).then(() => {
-                            location.replace('./?page=system_settings');
-                        });
-                    } else if(resp.status === 'failed' && resp.msg){
-                        var el = $('<div>');
-                        el.addClass("alert alert-danger err-msg").text(resp.msg);
-                        _this.prepend(el);
-                        el.show('slow');
-                        $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
-                        end_loader();
-                    } else {
-                        alert_toast("An error occurred", 'error');
-                        end_loader();
-                        console.log(resp);
-                    }
-                }
+                // Uncomment and customize the following success function as needed
+                // success: function(resp){
+                //     if(typeof resp === 'object' && resp.status === 'success'){
+                //         Swal.fire({
+                //             icon: 'success',
+                //             title: 'Success',
+                //             text: 'Information updated successfully!',
+                //             showConfirmButton: false,
+                //             timer: 1500
+                //         }).then(() => {
+                //             location.replace('./?page=system_settings');
+                //         });
+                //     } else if(resp.status === 'failed' && resp.msg){
+                //         var el = $('<div>');
+                //         el.addClass("alert alert-danger err-msg").text(resp.msg);
+                //         _this.prepend(el);
+                //         el.show('slow');
+                //         $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
+                //         end_loader();
+                //     } else {
+                //         alert_toast("An error occurred", 'error');
+                //         end_loader();
+                //         console.log(resp);
+                //     }
+                // }
             });
+        });
+
+        // Restrict phone input to numbers, dashes, and parentheses
+        $('#phone').on('input', function() {
+            this.value = this.value.replace(/[^0-9\(\)\-]/g, '');
         });
 
         // Restrict mobile input to numbers only
