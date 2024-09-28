77d5e46d1baf7d607f757aa7604adbbb63decb26
zetski <batuigasjerichodesu@gmail.com>
2024-09-28 09:31:06 +0800

    save

--- home.php
+++ home.php
@@ -62,7 +62,9 @@
                                 <hr class="bg-navy opacity-100" style="width:8em;height:3px;opacity:1">
                             </center>
                             <?= htmlspecialchars_decode(file_get_contents('./welcome.html')) ?>
-                                <button class="btn"><?= htmlspecialchars_decode(file_get_contents('./event.html')) ?>Upcoming Events </button>
+                            <button class="btn" onclick="window.location.href='./event.html';">
+                            <?= htmlspecialchars_decode(file_get_contents('./event.html')) ?>Upcoming Events
+                        </button>
                         </div>
                     </div>
                 </div>
