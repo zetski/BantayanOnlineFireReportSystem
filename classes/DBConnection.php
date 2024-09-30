c5f52323eeb89735e1191104a9a2a224b168f469
zetski <batuigasjerichodesu@gmail.com>
2024-09-30 21:30:07 +0800

    save

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -1,20 +1,20 @@
 <?php
 if(!defined('DB_SERVER')){
-    require_once("../initialize1.php");
+    require_once("../initialize.php");
 }
 class DBConnection{
 
     // LIVE SERVER
-    private $host = "127.0.0.1:3306";
-    private $username = "u510162695_ofrs_db";
-    private $password = "1Ofrs_db";
-    private $database = "u510162695_ofrs_db";
+    // private $host = "127.0.0.1:3306";
+    // private $username = "u510162695_ofrs_db";
+    // private $password = "1Ofrs_db";
+    // private $database = "u510162695_ofrs_db";
 
     // LOCALHOST
-    // private $host = DB_SERVER;
-    // private $username = DB_USERNAME;
-    // private $password = DB_PASSWORD;
-    // private $database = DB_NAME;
+    private $host = DB_SERVER;
+    private $username = DB_USERNAME;
+    private $password = DB_PASSWORD;
+    private $database = DB_NAME;
     
     public $conn;
     
@@ -22,16 +22,16 @@ class DBConnection{
 
         if (!isset($this->conn)) {
             
-            // Attempting to establish a connection to the database
             $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
             
-            // Check for connection errors and display detailed error message
-            if ($this->conn->connect_error) {
-                die('Connection failed: ' . $this->conn->connect_error);
-            }
+            if (!$this->conn) {
+                echo 'Cannot connect to database server';
+                exit;
+            } 
+                       
         }    
+        
     }
-
     public function __destruct(){
         $this->conn->close();
     }
