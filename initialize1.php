1dba4f424225f32ee3ef88507f307f55b3245334
zetski <batuigasjerichodesu@gmail.com>
2024-09-30 22:31:29 +0800

    save

--- initialize1.php
+++ initialize1.php
@@ -13,12 +13,14 @@
 //     die("Connection failed: " . $con->connect_error);
 // }
 
+
 // Base URL and application directory constants
 define('base_url', 'https://bantayan-bfp.com/');
 define('base_app', str_replace('\\', '/', __DIR__) . '/');
 
 // Database connection parameters
-define('DB_SERVER', "127.0.0.1:3306"); // or "localhost"
+define('DB_SERVER', "127.0.0.1"); // or "localhost"
+define('DB_PORT', 3306); // Optional, can be used if you want to be explicit
 define('DB_USERNAME', "u510162695_ofrs_db");
 define('DB_PASSWORD', "1Ofrs_db");
 define('DB_NAME', "u510162695_ofrs_db");
