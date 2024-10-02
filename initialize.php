5bb19a9db30383bacb2b987496d0b56369bdc541
zetski <batuigasjerichodesu@gmail.com>
2024-10-02 19:19:44 +0800

    save

--- initialize.php
+++ initialize.php
@@ -3,13 +3,7 @@ $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=
 
 // LIVE SERVER
 
-// if(!defined('base_url')) define('base_url','https://bantayan-bfp.com/');
-// if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
-// // if(!defined('dev_data')) define('dev_data',$dev_data);
-// if(!defined('DB_SERVER')) define('DB_SERVER',"127.0.0.1:3306");
-// if(!defined('DB_USERNAME')) define('DB_USERNAME',"u510162695_ofrs_db");
-// if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"1Ofrs_db");
-// if(!defined('DB_NAME')) define('DB_NAME',"u510162695_ofrs_db");
+
 
 // LOCALHOST 
 
