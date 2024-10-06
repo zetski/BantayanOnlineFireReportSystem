764381f2f48b0b6f2f087bb3131bc96b61a27232
zetski <batuigasjerichodesu@gmail.com>
2024-10-06 09:30:16 +0800

    save

--- admin/system_info/event_info.php
+++ admin/system_info/event_info.php
@@ -1,11 +1,6 @@
 <?php if($_settings->chk_flashdata('success')): ?>
     <script>
-        Swal.fire({
-            icon: 'success',
-            title: 'Success',
-            text: "<?php echo $_settings->flashdata('success') ?>",
-            confirmButtonText: 'OK'
-        });
+    alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success');
     </script>
 <?php endif; ?>
 
