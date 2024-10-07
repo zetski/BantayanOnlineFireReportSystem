8b38ea84b6179f2022eada798f5172b50aa19ae2
zetski <batuigasjerichodesu@gmail.com>
2024-10-07 13:33:26 +0800

    save

--- initialize.php
+++ initialize.php
@@ -13,13 +13,13 @@ $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=
 
 // LOCALHOST 
 
-if(!defined('base_url')) define('base_url','http://localhost/ofrs/');
-    if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
-    // if(!defined('dev_data')) define('dev_data',$dev_data);
-    if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-    if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-    if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-    if(!defined('DB_NAME')) define('DB_NAME',"ofrs_db");
+// if(!defined('base_url')) define('base_url','http://localhost/ofrs/');
+//     if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+//     // if(!defined('dev_data')) define('dev_data',$dev_data);
+//     if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+//     if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+//     if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+//     if(!defined('DB_NAME')) define('DB_NAME',"ofrs_db");
     // Establish the connection
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
