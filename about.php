0efc49d9da98dd3ce09e9900a4a9fb5b326a5512
zetski <batuigasjerichodesu@gmail.com>
2024-10-12 09:26:29 +0800

    save

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="card rounded-0">
             <div class="card-body">
-                <?= htmlspecialchars_decode(file_get_contents('./aboutB.html')) ?>
+                <?= htmlspecialchars_decode(file_get_contents('./about.html')) ?>
             </div>
         </div>
     </div>
