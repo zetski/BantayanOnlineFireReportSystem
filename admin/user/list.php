f81a90232a94d8bddd0a36fbff952a4fb7ecc583
zetski <batuigasjerichodesu@gmail.com>
2024-10-12 20:28:07 +0800

    save

--- admin/user/list.php
+++ admin/user/list.php
@@ -14,9 +14,9 @@
 <div class="card card-outline rounded-0 card-danger">
 	<div class="card-header">
 		<h3 class="card-title">List of Users</h3>
-		<!-- <div class="card-tools">
+		<div class="card-tools">
 			<a href="./?page=user/manage_user" id="create_new" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
-		</div> -->
+		</div>
 	</div>
 	<div class="card-body">
         <div class="container-fluid">
@@ -37,8 +37,8 @@
 						<th>Avatar</th>
 						<th>Name</th>
 						<th>Username</th>
-						<th>District</th>
 						<th>Type</th>
+						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -55,7 +55,6 @@
                             </td>
 							<td><?php echo $row['name'] ?></td>
 							<td><?php echo $row['username'] ?></td>
-							<td><?php echo $row['district'] ?></td>
 							<td class="text-center">
                                 <?php if($row['type'] == 1): ?>
                                     Administrator
@@ -64,18 +63,18 @@
                                 <?php else: ?>
 									N/A
                                 <?php endif; ?>
-                            <!-- </td>
+                            </td>
 							<td align="center">
 								 <button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
 				                  		Action
 				                    <span class="sr-only">Toggle Dropdown</span>
 				                  </button>
 				                  <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item" href="./?page=user/manage_user&id=</?= $row['id'] ?>"><span class="fa fa-edit text-dark"></span> Edit</a>
+				                    <a class="dropdown-item" href="./?page=user/manage_user&id=<?= $row['id'] ?>"><span class="fa fa-edit text-dark"></span> Edit</a>
 				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="</?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
 				                  </div>
-							</td> -->
+							</td>
 						</tr>
 					<?php endwhile; ?>
 				</tbody>
