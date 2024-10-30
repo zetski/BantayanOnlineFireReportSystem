3fe9f451c90f09c39fda38ded7c452fc31dd73b1
zetski <batuigasjerichodesu@gmail.com>
2024-10-31 03:30:35 +0800

    change

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -19,19 +19,15 @@ class DBConnection{
     public $conn;
     
     public function __construct(){
-
         if (!isset($this->conn)) {
-            
             $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
             
-            if (!$this->conn) {
-                echo 'Cannot connect to database server';
-                exit;
+            if ($this->conn->connect_error) {
+                die('Connection failed: ' . $this->conn->connect_error);
             } 
-                       
         }    
-        
     }
+    
     public function __destruct(){
         $this->conn->close();
     }
