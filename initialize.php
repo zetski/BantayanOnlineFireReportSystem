6de13cb1ab9a066f429957894c5f38fa1ecd8b84
zetski <batuigasjerichodesu@gmail.com>
2024-11-01 00:37:07 +0800

    save

--- initialize.php
+++ initialize.php
@@ -1,5 +1,4 @@
 <?php
-// $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
 
 // LIVE SERVER
 
